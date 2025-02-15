c06cfa2267aaf4a4cea3e101f059d9fd474c35ae
Jouf <143747820+joufalahmad@users.noreply.github.com>
2025-02-15 19:39:35 +0300

    update_choose_mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+type product={
+'id':number,
+'name':string,
+'price':number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myApp';
+  title='';
+  phone=[
+    {'id':1,'name':'iphone 14 pro','price':3000},
+    {'id':2,'name':'samsunf s24','price':1000},
+    {'id':3,'name':'nokia','price':1500},
+    {'id':4,'name':'sony','price':1200}
+
+
+
+  ]
+  search:string="";
+
+  find(phone:any[]){
+
+return phone.filter(item => item.name.toLowerCase().includes(this.search.toLowerCase()))
+  }
 }
